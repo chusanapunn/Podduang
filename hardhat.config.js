faf3dbdb14ad8c092ac966ebf82b750b3d2c0592
chusana.r@outlook.com <chusana.r@outlook.com>
2023-11-23 20:20:00 +0700

    hardhat first contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+  compilers: [
+    {
+      version: "0.8.1"
+    },
+    {
+      version: "0.8.20"
+    },
+  ]
+},
+  networks: {
+    localganache:{
+      url: process.env.ALCHEMY_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  // etherscan: {
+    // apiKey: process.env.API_KEY
+  // }
+
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Podduang = await hre.ethers.getContractFactory("Podduang");
+  const podduang = await Podduang.deploy("PodduangName","ONLY1");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await podduang.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log('Podduang deployed to:',podduang.target);
 
-  await lock.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
